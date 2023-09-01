d428baf11bffd69696dcf00261db83d24cfb9d3a
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-01 12:35:36 +0200

    styling item grid

--- src/components/CategoryGridTile.js
+++ src/components/CategoryGridTile.js
@@ -4,9 +4,10 @@ import { Pressable, View, Text, StyleSheet } from "react-native";
 function CategoryGridTile ({title, color}){
   return( 
     <View style={styles.gridItem}>
-      <Pressable>
-        <Text>{title}</Text>
-
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
       </Pressable>
     </View>
   )
@@ -16,10 +17,28 @@ export default CategoryGridTile;
 
 const styles = StyleSheet.create({
   gridItem: {
-    flex: 1,
+    flex: 1, // It takes all available space
     margin: 16,
     height:150,
     borderRadius: 8,
-    elevation: 4, //This is for Android devices , a shadow :)
+    elevation: 4, //This works in ANDROID device , a shadow :)
+    backgroundColor:'white', //This fix shadow problem in IOS device
+    shadowColor: 'black', //This works in IOS device , a shadow :)
+    shadowOpacity:0.25, //This works in IOS device , a shadow :)
+    shadowOffset:{width:0, height:2}, //This works in IOS device , a shadow :)
+    shadowRadius:8, //This works in IOS device , a shadow :)
   },
+  innerContainer: {
+    flex: 1, // It takes all available space
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 1,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  }
 });
