d90668e600083e91daa8bba48ee0c4609ca471ae
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-01 18:47:30 +0200

    Installing native stack dependency

--- App.js
+++ App.js
@@ -2,17 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 
 import CategoriesScreen from './src/screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator(); // An object with properties
 
 export default function App() {
   return (
     <>
       <StatusBar style='light'/>
-        <NavigationContainer>
-          <CategoriesScreen/>
-        </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name='MealCategories'
+            component={CategoriesScreen}
+          />
+        </Stack.Navigator>
+
+        <CategoriesScreen/>
+      </NavigationContainer>
     </>
   );
 }
