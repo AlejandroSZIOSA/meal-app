b36a9620030c8a078ac67c67b11c095291f9a260
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-02 13:49:49 +0200

    Moving FN renderCategoryItem into FN CategoriesScreen

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -2,25 +2,25 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function pressHandler() {
 
-  
-}
 
-//The form of how the item will renders :)
+
+//Destructuring Navigation Obj gives a method for navigate between screens
+function CategoriesScreen({navigation}){
+
+  //The form of how the item will renders :)
 function renderCategoryItem(itemData){
+  function pressHandler() {
+  }
   return (
     <CategoryGridTile 
       title={itemData.item.title} 
       color={itemData.item.color}
       //Callback connect and pointing when a category es pressed
       onPress={pressHandler}
-
     />
   )
 }
-
-function CategoriesScreen(){
 
   return (
   <FlatList 
