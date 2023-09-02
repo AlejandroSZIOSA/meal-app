267e31d717aa476be1e48efcde5b21f7d0ac28e4
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-02 13:22:16 +0200

    Create MealsOverviewScreen

---
+++ src/screens/MealsOverviewScreen.js
@@ -0,0 +1,19 @@
+import { View, StyleSheet} from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+function MealsOverviewScreen(){
+  return(
+  <View style={styles.container}>
+    <Text>Meals Overview Screen</Text>
+  </View>
+  );
+}
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex:16,
+    padding: 16,
+  }
+
+})
