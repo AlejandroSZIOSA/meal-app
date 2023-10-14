a2c35aedff9a01876eb66c30d5c66962de10ed25
AlejandroSZIOSA <ggato33@gmail.com>
2023-10-14 18:49:38 +0200

    Settings Detail Meal Screen

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,5 +1,6 @@
 import { View,Pressable,Text, Image, StyleSheet,Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 function MealItem ({id,title, imageUrl, duration,complexity, affordability}){
 
@@ -27,12 +28,13 @@ function MealItem ({id,title, imageUrl, duration,complexity, affordability}){
             <Image source ={{uri: imageUrl}} style={styles.image}/>
             <Text style={styles.title}>{title}</Text>
           </View>
-
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m </Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          {/* Detail Screen */}
+          <MealDetails 
+            duration={duration} 
+            affordability={affordability} 
+            complexity={complexity}
+            />
+          
         </View>
       </Pressable>
     </View>
@@ -69,18 +71,6 @@ const styles = StyleSheet.create({
     textAlign:'center',
     fontSize:18,
     padding:8,
-  },
-
-  details:{
-    flexDirection:'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:8,
-  },
-  
-  detailItem:{
-    marginHorizontal:4,
-    fontSize: 12,
   },
 
   buttonPressed:{
